ef48ca4be7cef4ea877ca919d3c1fbae14d8312d
smartdev <smartdevs17@gmail.com>
2024-09-09 08:20:17 +0100

    updated endpoints and added lunch icons

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -9,12 +9,12 @@ class Constants {
   //Myhot Spot url
   // static const String baseUrl = "http://192.168.225.80:3000/api";
   //MyAirtel url
-  static const String baseUrl = "http://192.168.1.195:3000/api";
+  // static const String baseUrl = "http://192.168.1.195:3000/api";
   //MyMtn Mifi
   // static const String baseUrl = "http://192.168.0.195:3000/api";
 
   //Server Error
-  // static const String baseUrl = "https://writeapp-api.onrender.com/api";
+  static const String baseUrl = "https://madang-api.onrender.com/api";
   // AUTH
   static const String STGW = 'Something gone wrong';
   static const String USER_EXIST = 'User already exist';
